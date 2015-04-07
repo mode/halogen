a71e79a07776706237f839c06e1607d3ed6201b3
Heather Rivers <heather@modeanalytics.com>
2015-04-07 12:57:24 -0700

    Add version badge and remove coverage reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,11 +5,6 @@ SimpleCov.start do
   add_filter '/spec'
 end
 
-if ENV['CODECLIMATE_REPO_TOKEN']
-  require 'codeclimate-test-reporter'
-  CodeClimate::TestReporter.start
-end
-
 require 'bundler/setup'
 Bundler.setup
 
